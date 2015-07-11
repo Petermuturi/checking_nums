eefaef6fc761aff67fcd10efd0b333de4aa37ad9
Peter Muturi <pmuturi65@gmail.com>
2015-07-11 14:32:21 +0300

    comparingnms

---
+++ checknums.rb
@@ -0,0 +1,16 @@
+
+
+def checkNums()
+puts "Enter first number"
+no1= gets.chomp
+puts "Enter second number"
+no2= gets.chomp
+
+if no1 > no2
+puts "#{no1} is greater"
+else
+puts "#{no2} is greater"
+end
+end
+puts checkNums()
+
